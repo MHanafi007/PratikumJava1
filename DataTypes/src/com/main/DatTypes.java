52a55ffd2768efe57367347bce191a7913ea8ee9
MHanafi007 <hanafiayanokouji@gmail.com>
2021-06-13 06:49:03 +0800

    deklarasi vasriabel dan tipe data

---
+++ DataTypes/src/com/main/DatTypes.java
@@ -0,0 +1,15 @@
+package com.main;
+
+public class DatTypes {
+    public static void main(String [] args){
+        String namaDepan = "Mochammad";
+        String namaTengah = "Hanafi";
+        String namaBelakang = "Sky";
+        int usia = 20;
+        int targetTahunKuliah = 4;
+        double ipk = 3.89;
+        char nilaiAbjad = 'A';
+        boolean tanpan = true;
+
+    }
+}
