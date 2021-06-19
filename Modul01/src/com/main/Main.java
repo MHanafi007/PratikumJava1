c5bfa2fac6938a922e33a3321b7ff8df183b69f8
MHanafi007 <hanafiayanokouji@gmail.com>
2021-06-20 04:54:07 +0800

    Membuat String dari Array Char

--- Modul01/src/com/main/Main.java
+++ Modul01/src/com/main/Main.java
@@ -10,8 +10,10 @@ public class Main {
 	int targetTahunKuliah = 4;
 	double ipk = 3.89764512;
 	boolean tanpan = true;
+	char[] uniskaChar = {'U','N','I','S','K','A'};
+	String uniskaString = new String (uniskaChar);
 
-
+	System.out.println(uniskaChar);
 	System.out.println(namaDepan + namaTengah + namaBelakang);
     }
 }
