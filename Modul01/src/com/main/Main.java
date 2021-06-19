04d5137e5118419ab856c735d14d444cb54bd62d
MHanafi007 <hanafiayanokouji@gmail.com>
2021-06-20 05:04:02 +0800

    Operasi Tipe String

--- Modul01/src/com/main/Main.java
+++ Modul01/src/com/main/Main.java
@@ -1,5 +1,7 @@
 package com.main;
 
+import java.util.Locale;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -12,8 +14,24 @@ public class Main {
 	boolean tanpan = true;
 	char[] uniskaChar = {'U','N','I','S','K','A'};
 	String uniskaString = new String (uniskaChar);
+	String namaLengkap = namaDepan + " " + namaTengah + " " + namaBelakang;
+
 
 	System.out.println(uniskaChar);
 	System.out.println(namaDepan + namaTengah + namaBelakang);
+	System.out.println(namaLengkap.indexOf("NIS"));
+	System.out.println(namaLengkap.substring(5));
+	System.out.println(namaLengkap.substring(5,12));
+	System.out.println(namaLengkap.replace("Yog", "Barr"));
+	System.out.println(namaLengkap.toUpperCase());
+	System.out.println(namaLengkap.toLowerCase());
+	System.out.println(namaLengkap.charAt(6));
+	String[] namaArray = namaLengkap.split(" ");
+
+	for(String nama : namaArray){
+		System.out.println(nama);
+	}
+
+
     }
 }
