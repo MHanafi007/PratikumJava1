383ed18c3a3c44410cd74fc3d97edc16642c0790
MHanafi007 <hanafiayanokouji@gmail.com>
2021-06-20 04:51:05 +0800

    Project IDEA

---
+++ Modul01/src/com/main/Main.java
@@ -0,0 +1,17 @@
+package com.main;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String namaDepan = "Mirza";
+	String namaTengah = "Yogy";
+	String namaBelakang = "Kurniawan";
+	int usia = 18;
+	int targetTahunKuliah = 4;
+	double ipk = 3.89764512;
+	boolean tanpan = true;
+
+
+	System.out.println(namaDepan + namaTengah + namaBelakang);
+    }
+}
